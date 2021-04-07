55168a2c794de239ad297f53be413a9dbf8ec0e5
hoyame <zou.hoyame@gmail.com>
2021-04-07 19:06:15 +0200

    app: add note

---
+++ src/views/os/Notes.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <HeaderApp title="Notes" left="" right="Nouveau" search="true" />
+        
+		<div class="app-container">
+            <div class="item">
+                <div class="title">dJaaja</div>
+                <div class="desc">Ya pas moyen djaja</div>
+            </div>
+
+            <div class="item">
+                <div class="title">Chui</div>
+                <div class="desc">pas ta catin djaja genre</div>
+            </div>
+        </div>
+        <div @click="goHome" class="controlbar"></div>
+
+    </div>
+</template>
+
+<script>
+import HeaderApp from '../../components/os/HeaderApp';
+
+export default {
+    name: 'Notes',
+
+    components: {
+        HeaderApp
+    },
+
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+.app-container {
+    padding-top: 7.5px;
+}
+
+.item {
+    margin: 0 auto;
+    margin-top: 7.5px;
+    height: 32px;
+    width: 82%;
+    background-color: #fff;
+    border-radius: 8px;
+    font-family: "SF-Pro-Text-Regular";
+    padding: 10px;
+    transition-duration: 300ms;
+}   
+
+.title {
+    font-size: 14px;
+}
+
+.desc {
+    color: grey;
+    font-size: 12px;
+}
+
+.item:hover {
+    cursor: pointer;
+    background-color: rgb(228, 227, 227);
+}
+</style>
