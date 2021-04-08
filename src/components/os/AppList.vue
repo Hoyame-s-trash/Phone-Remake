e2fd19cba8133dd25140ff591dfdcc8d2143d193
hoyame <zou.hoyame@gmail.com>
2021-04-08 13:43:21 +0200

    app: add music support

--- src/components/os/AppList.vue
+++ src/components/os/AppList.vue
@@ -3,14 +3,14 @@
         <AppIcon @click.native="openF('MessageList')" class="icon" title='Message' img="messages.png" />
         <AppIcon class="icon" title='Mail' img="mail.png" />
         <AppIcon @click.native="openF('CallList')" class="icon" title='Appels' img="call.png" />
-        <AppIcon @click.native="openF('ContactEdit')" class="icon" title='Whatsapp' img="whatsapp.png" />
+        <AppIcon @click.native="openF('ContactList')" class="icon" title='Whatsapp' img="whatsapp.png" />
         <AppIcon @click.native="openF('Calculator')" class="icon" title='Calculette' img="calculator.png" />
         <AppIcon class="icon" title='Camera' img="camera.png" />
         <AppIcon @click.native="openF('ContactList')" class="icon" title='Contacts' img="contacts.png" />
-        <AppIcon @click.native="openF('NewNotes')" class="icon" title='Notes' img="notes.png" />
+        <AppIcon @click.native="openF('Notes')" class="icon" title='Notes' img="notes.png" />
         <AppIcon class="icon" title='Horloge' img="clock.png" />
         <AppIcon class="icon" title='Plans' img="maps.png" />
-        <AppIcon class="icon" title='News' img="news.png" />
+        <AppIcon @click.native="openF('Music')" class="icon" title='Musique' img="music.png" />
         <AppIcon @click.native="openF('Gallery')" class="icon" title='Galerie' img="photos.png" />
         <AppIcon @click.native="openF('Settings')" class="icon" title='Réglages' img="settings.png" />
     </div>
